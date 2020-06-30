9677b53f98b5ddacf232da82b00c4485eb0b4a4a
rmdpalo <rmd.palo@gmail.com>
2020-06-30 16:30:46 -0700

    uncommented other challenges

--- selcynmig-arrays.js
+++ selcynmig-arrays.js
@@ -1,35 +1,35 @@
-// var groceryList = ["chips", "dip", "cookies"]
-//
-// groceryList.push("soda")
-// console.log("original list " + groceryList)
-// var newGroceryList = groceryList
-// newGroceryList.push("granola")
-//
-// // var newGroceryList = groceryList.push("granola")
-//
-// console.log("original list " + groceryList)
-// console.log("new list " + newGroceryList)
-// //challenge 2
-// var numbers = [2, 4, 6, 8, 10]
-// numbers.unshift(0)
-// console.log(numbers);
-// numbers.push(12)
-// console.log(numbers);
-// numbers.shift()
-// console.log(numbers);
-//
-// var numbers2 = [0]
-//
-// var combineNumbers = numbers2.concat(numbers)
-// console.log(combineNumbers);
-//
-//challenge 3
-// var numSet = [2, 13, 6, 8, 4, 2]
-//
-// //Write the code that finds the index of the first appearance of the number 2.
-// console.log(numSet.indexOf(2));
-// //Write the code that finds the index of the last appearance of the number 2.
-// console.log(numSet.lastIndexOf(2));
+var groceryList = ["chips", "dip", "cookies"]
+
+groceryList.push("soda")
+console.log("original list " + groceryList)
+var newGroceryList = groceryList
+newGroceryList.push("granola")
+
+// var newGroceryList = groceryList.push("granola")
+
+console.log("original list " + groceryList)
+console.log("new list " + newGroceryList)
+//challenge 2
+var numbers = [2, 4, 6, 8, 10]
+numbers.unshift(0)
+console.log(numbers);
+numbers.push(12)
+console.log(numbers);
+numbers.shift()
+console.log(numbers);
+
+var numbers2 = [0]
+
+var combineNumbers = numbers2.concat(numbers)
+console.log(combineNumbers);
+
+challenge 3
+var numSet = [2, 13, 6, 8, 4, 2]
+
+//Write the code that finds the index of the first appearance of the number 2.
+console.log(numSet.indexOf(2));
+//Write the code that finds the index of the last appearance of the number 2.
+console.log(numSet.lastIndexOf(2));
 
 //challenge 4
  var chars = ["y", "a", "r", "r", "a"]
