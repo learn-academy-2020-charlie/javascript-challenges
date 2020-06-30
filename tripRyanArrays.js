01171fe801e96826a0e135e4c82eba35b5753949
ryankoch13 <ryan.koch13@gmail.com>
2020-06-30 15:52:03 -0700

    adding our file and first 2 challenges

---
+++ tripRyanArrays.js
@@ -0,0 +1,42 @@
+// Consider this variable:
+//   var groceryList = ["chips", "dip", "cookies"]
+// Write the code that will add "soda" to the end of the original array.
+// Write the code that will add "granola" to the end of array without altering the original array.
+
+
+//   var numbers = [2, 4, 6, 8, 10]
+// Write the code that will add the number 0 to the beginning of the array.
+
+// Write the code that will add the number 12 to the end of the array.
+
+// Write the code that will remove the first number from the array.
+
+// Write the code that will add the number 0 to the beginning of the array without altering the original array.
+
+// var groceryList = ["chips", "dip", "cookies"]
+// groceryList.push("soda");
+
+// // console.log(groceryList);
+
+// var newGroceryList = groceryList.concat("granola");
+
+
+// console.log(newGroceryList);
+
+// // console.log(groceryList);
+
+// var nums = [2, 4, 6, 8 , 10]
+
+// nums.unshift(0);
+
+
+// nums.push(12);
+
+// nums.shift();
+
+// // console.log(nums);
+
+// var newNums = [0];
+
+// console.log(newNums.concat(nums));
+
