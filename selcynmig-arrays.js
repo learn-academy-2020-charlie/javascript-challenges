0ec8b20c6266195cd6cc42c535edcba41026660d
cynavago <cnavarrohdz@gmail.com>
2020-06-30 15:57:54 -0700

    challenge 1

---
+++ selcynmig-arrays.js
@@ -0,0 +1,11 @@
+var groceryList = ["chips", "dip", "cookies"]
+
+groceryList.push("soda")
+console.log("original list " + groceryList)
+var newGroceryList = groceryList
+newGroceryList.push("granola")
+
+// var newGroceryList = groceryList.push("granola")
+
+console.log("original list " + groceryList)
+console.log("new list " + newGroceryList)
