11a8f9c29a48c3d2d4ed52174c75219650becb39
selamhabteab <selamhabteab@gmail.com>
2020-06-30 14:47:29 -0700

    1-4 challenges

---
+++ selcyn.js
@@ -0,0 +1,36 @@
+1st challenge
+var purse= 99
+if (purse<=100) {
+  console.log("In budget!");
+}
+2nd challenge
+var hunger = false
+
+if (hunger == true){
+  console.log("eat food");
+} else {
+  console.log("keep coding");
+}
+3rd challenge
+var trafficLight = "red"
+if (trafficLight == "green") {
+  console.log("Go!");
+}
+else if (trafficLight=="yellow") {
+  console.log("Slow down!");
+}
+else {
+  console.log("stop");
+}
+//4th challenge
+var cynthiaAge = 35;
+var selamAge = 29;
+if (cynthiaAge > selamAge){
+  console.log(cynthiaAge);
+}
+else if (cynthiaAge === selamAge){
+  console.log("Same age!");
+}
+else {
+  console.log(selamAge);
+}
