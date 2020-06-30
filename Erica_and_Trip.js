64ae067fb685505d86fcd9d2c869702f99174cb0
ericagates <ericajoangates@gmail.com>
2020-06-30 14:17:20 -0700

    first commit

---
+++ Erica_and_Trip.js
@@ -0,0 +1,18 @@
+var item = 101;
+if (item <= 100) {
+    console.log("in budget");
+}
+var hungry = false;
+if (hungry) {
+    console.log("eat food");
+} else {
+    console.log("keep coding");
+}
+var trafficLight = "red";
+if (trafficLight == "green") {
+    console.log("go");
+} else if(trafficLight == "yellow") {
+    console.log('slow down');
+} else if (trafficLight == "red") {
+    console.log("stop")
+}
