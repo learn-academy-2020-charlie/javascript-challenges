ed018eac4045c1b4cb31d434a0e442833a0d32b7
selamhabteab <selamhabteab@gmail.com>
2020-07-01 10:21:10 -0700

    2nd addition to challenge

--- migryanselam-loops.js
+++ migryanselam-loops.js
@@ -1,16 +1,40 @@
+//
+// for (let i = 0; i <= 20; i++){
+//   console.log(i);
+// }
+//
+// for (let i = 0; i <= 20; i++){
+//   console.log(i * 3);
+// }
+//
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 2 !== 0) {
+//     console.log("ODD");
+// } else {
+//   console.log(i);
+// }
+// }
+//2nd addition
+// var nums = [3, 57, -9, 20, 67]
+// var high = 0
+// for (let i=0; i<nums.length; i++){
+// if (nums[i]>high){
+//   high=nums[i]
+// }
+// }
+// console.log(high);
 
-for (let i = 0; i <= 20; i++){
-  console.log(i);
-}
+// var nums = [3, 57, -9, 20, 67]
+// var low = 0
+// for (let i=0; i<nums.length; i++){
+// if (nums[i]<low){
+//   low=nums[i]
+// }
+// }
+// console.log(low);
 
-for (let i = 0; i <= 20; i++){
-  console.log(i * 3);
-}
-
-for (let i = 0; i <= 20; i++) {
-  if (i % 2 !== 0) {
-    console.log("ODD");
-} else {
-  console.log(i);
-}
+var nums = [3, 57, -9, 20, 67]
+for (let i=0; i<nums.length; i++){
+var remainder = nums[i]%2
+console.log(remainder);
 }
