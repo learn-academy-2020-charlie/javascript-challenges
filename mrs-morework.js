b73f2a94ec14f8fc412905cd57efad3e33587e23
rmdpalo <rmd.palo@gmail.com>
2020-07-01 15:58:07 -0700

    started challenges

--- mrs-morework.js
+++ mrs-morework.js
@@ -2,12 +2,18 @@
 // Expected output --> [9, 27, 45, 12, 30]
 
 var testArr1 = [3, 9, 15, 4, 10]
+//created a function
 const newArray = (array) => {
+  //new array to store multiplied numbers
   let multiplied = []
-
-  array * 3
+  //for loop that goes through every index of original array to multiply and pushes to new array
+  for (i = 0; i < array.length; i++){
+    multiplied.push(array[i] * 3)
+  }
+  //returns the multiplied array
+  return multiplied
 }
-console.log();
+console.log(newArray(testArr1));
 //(2) Write a function that takes in an array and returns a new array with only odd numbers.
 // Expected output --> [-7, 3, 5, 13]
 var testArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
