f41e315b0af3ef41a70499e8d03803eb50702126
ericagates <ericajoangates@gmail.com>
2020-07-01 13:57:15 -0700

    functions 1

--- forloops_e_and_j.js
+++ forloops_e_and_j.js
@@ -27,7 +27,7 @@
 // Create a loop that will log the highest number from the array. Expected output --> 67
 // Create a loop that will log the lowest number from the array Expected output --> -9
 // Create a loop that will log the remainder of each number when divided by 2. Expected output --> 1, 1, -1, 0, 1
-var nums = [3, 57, -9, 20, 67]
+var nums = [3, 57, -9, 20, -20]
 var highestNum = null;
 
 for (var i = 0; i < nums.length; i++) {
@@ -38,4 +38,42 @@ for (var i = 0; i < nums.length; i++) {
   }
 }
 
-console.log(highestNum);
+//console.log(highestNum);
+
+
+//Challenge 1 FUNCTIONS//
+//Write a function named marco that returns "polo".
+
+// const marco = () => {
+//   return "polo"
+// }
+//
+// console.log(marco());
+
+//2
+//Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+
+// function named greeting with name as argument
+// const greeting = (name) => {
+//   // returns "Welcome, <person's name here!>"
+//   return `Welcome, ${ name }!`
+// }
+//
+// console.log(greeting("Jakob"));
+
+//3
+//Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+
+//function named oddOrEven that takes number argument
+// const oddOrEven = (number) => {
+//     // modulo remainder not equal to 0
+//     if (number % 2 !== 0){
+//       //return odd
+//       return "odd"
+//     } else {
+//       //return even
+//       return "even"
+//     }
+// }
+//
+// console.log(oddOrEven(73));
