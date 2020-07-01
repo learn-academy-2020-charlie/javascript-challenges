6f55d0e8597ead3bdf22b923413f65a79e3b2623
rmdpalo <rmd.palo@gmail.com>
2020-07-01 14:06:24 -0700

    finished function challenges

--- migryanselam-functions.js
+++ migryanselam-functions.js
@@ -24,14 +24,41 @@
 
 
 //(4) Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
-
-const triple = (num) => {
-  return num * 3;
-}
-console.log(triple(9))
+//
+// const triple = (num) => {
+//   return num * 3;
+// }
+// console.log(triple(9))
 
 //(5) Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
+// const multiply = (num1, num2) => {
+//   return num1 * num2
+// }
+// console.log(multiply(3, 4));
 
 //(6) Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
 
+// const divisibleBy = (num1, num2) => {
+//   if (num1 % num2 === 0) {
+//     return `${num1} is evenly divisble by ${num2}`
+//   }
+// }
+// console.log(divisibleBy(8, 4));
+
+
 //(7) Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+
+const assignGrade = (num) => {
+  if (num >= 90) {
+    return "A"
+  } else if (num >= 80) {
+    return "B"
+  } else if (num >= 70) {
+    return "C"
+  } else if (num >= 60) {
+    return "D"
+  } else {
+    return "F"
+  }
+}
+console.log(assignGrade(30));
