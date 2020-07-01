46d9efed702f4818a66d327f6c8561cb5fe99f45
ryankoch13 <ryan.koch13@gmail.com>
2020-07-01 13:54:45 -0700

    challenges 3-4

--- migryanselam-functions.js
+++ migryanselam-functions.js
@@ -1,17 +1,34 @@
 //Function Challenges
 //(1) Write a function named marco that returns "polo".
- const marco = () => {
-   return "polo"
- }
- console.log(marco());
-//(2) Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
-const greeting = (name) => {
-  return `Welcome, ${name}!`
-}
-console.log(greeting("Miguel, Ryan, and Selam"));
+//  const marco = () => {
+//    return "polo"
+//  }
+//  console.log(marco());
+// //(2) Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+// const greeting = (name) => {
+//   return `Welcome, ${name}!`
+// }
+// console.log(greeting("Miguel, Ryan, and Selam"));
 //(3) Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
-//const oddOrEven
+
+
+// const oddOrEven = (num) => {
+//   if (num % 2 == 0) {
+//     return "even";
+//   } else {
+//     return "odd";
+//   }
+// }
+// console.log(oddOrEven(6));
+
+
+
 //(4) Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+
+const triple = (num) => {
+  return num * 3;
+}
+console.log(triple(9))
 
 //(5) Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
 
