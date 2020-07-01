baa8ce3b7f3762c7563d809e1c94d717ac455330
cynavago <cnavarrohdz@gmail.com>
2020-07-01 16:01:17 -0700

    challenge 2

--- TCR-functions.js
+++ TCR-functions.js
@@ -51,27 +51,48 @@
 // What number's bigger: Write a function named greaterNum that takes 2 arguments, both numbers and returns whichever number is the greater (higher) number
 
 //Write/declare a function named greaterNum with 2 args
-const greaterNum = (num1, num2) => {
-//if statement comparing the two args
-//If a > b, return a
-    if (num1 > num2){
-        return num1;
-//Else if a < b, return b
-    } else if (num1 < num2) {
-        return num2;
-//Else return, "Both numbers are equal"
-    } else {
-        return "The numbers are equal"
-    }
-}
-
-console.log("This is test 1: " + greaterNum(1000000, 1));
-console.log("This is test 2: " + greaterNum(1, 500));
-console.log("This is test 3: " + greaterNum(4, 4));
-
-
+// const greaterNum = (num1, num2) => {
+// //if statement comparing the two args
+// //If a > b, return a
+//     if (num1 > num2){
+//         return num1;
+// //Else if a < b, return b
+//     } else if (num1 < num2) {
+//         return num2;
+// //Else return, "Both numbers are equal"
+//     } else {
+//         return "The numbers are equal"
+//     }
+// }
+//
+// console.log("This is test 1: " + greaterNum(1000000, 1));
+// console.log("This is test 2: " + greaterNum(1, 500));
+// console.log("This is test 3: " + greaterNum(4, 4));
 
 
+// #2 Challenge
+// The World Translator: Write a function named helloWorld that takes 1 argument, a language code (e.g. "es", "de", "en") and returns "Hello, World" for the given language, for at least 3 languages (it should default to returning English)
 
-
-
+// //Declare the function name helloWorld and give it 1 arg
+// const helloWorld = (language) => {
+// //If the argument is null, default to English
+//   if (language == null){
+//     return "Hello, World";
+//   }
+// //If statement for the first language variation, return "Hello, World" in that language
+//   if (language == "es"){
+//     return "Hola, Mundo";
+//   }
+// //If statement for the second language variation, return "Hello, World" in that language
+//   if (language == "it"){
+//     return "Ciao mondo";
+//   }
+// //If statement for the third language variation, return "Hello, World" in that language
+//   if (language == "rs"){
+//     return "Привет мир";
+//     //Else, "Language not available"
+//   } else {
+//     return "Language not available"
+//   }
+// }
+// console.log(helloWorld("fs"));
