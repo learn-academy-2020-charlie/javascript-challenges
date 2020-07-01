c73d023ded1409bc5270be69456a0adca13fa865
rmdpalo <rmd.palo@gmail.com>
2020-07-01 10:48:31 -0700

    finished stretch challenges

--- migryanselam-loops.js
+++ migryanselam-loops.js
@@ -1,54 +1,54 @@
-//
-// for (let i = 0; i <= 20; i++){
-//   console.log(i);
-// }
-//
-// for (let i = 0; i <= 20; i++){
-//   console.log(i * 3);
-// }
-//
-// for (let i = 0; i <= 20; i++) {
-//   if (i % 2 !== 0) {
-//     console.log("ODD");
-// } else {
-//   console.log(i);
-// }
-// }
-//2nd addition
-// var nums = [3, 57, -9, 20, 67]
-// var high = 0
-// for (let i=0; i<nums.length; i++){
-// if (nums[i]>high){
-//   high=nums[i]
-// }
-// }
-// console.log(high);
 
-// var nums = [3, 57, -9, 20, 67]
-// var low = 0
-// for (let i=0; i<nums.length; i++){
-// if (nums[i]<low){
-//   low=nums[i]
-// }
-// }
-// console.log(low);
+for (let i = 0; i <= 20; i++){
+  console.log(i);
+}
 
-// var nums = [3, 57, -9, 20, 67]
-// for (let i=0; i<nums.length; i++){
-// var remainder = nums[i]%2
-// console.log(remainder);
-// }
+for (let i = 0; i <= 20; i++){
+  console.log(i * 3);
+}
 
-// var myString = "learn student";
-// var myArray = myString.split("");
-// var counter = 0;
-// // console.log(myArray);
-// for ( i=0 ; i<myArray.length ; i++ ) {
-//     if (myArray[i] === "e") {
-//         counter++;
-//     }
-// }
-// console.log(counter);
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 !== 0) {
+    console.log("ODD");
+} else {
+  console.log(i);
+}
+}
+2nd addition
+var nums = [3, 57, -9, 20, 67]
+var high = 0
+for (let i=0; i<nums.length; i++){
+if (nums[i]>high){
+  high=nums[i]
+}
+}
+console.log(high);
+
+var nums = [3, 57, -9, 20, 67]
+var low = 0
+for (let i=0; i<nums.length; i++){
+if (nums[i]<low){
+  low=nums[i]
+}
+}
+console.log(low);
+
+var nums = [3, 57, -9, 20, 67]
+for (let i=0; i<nums.length; i++){
+var remainder = nums[i]%2
+console.log(remainder);
+}
+
+var myString = "learn student";
+var myArray = myString.split("");
+var counter = 0;
+// console.log(myArray);
+for ( i=0 ; i<myArray.length ; i++ ) {
+    if (myArray[i] === "e") {
+        counter++;
+    }
+}
+console.log(counter);
 
 for (let i = 0; i <= 15; i++) {
       if (i % 2 !== 0) {
@@ -56,4 +56,16 @@ for (let i = 0; i <= 15; i++) {
     } else {
       console.log(i + " IS EVEN!");
     }
+}
+
+for (let i = 1; i <=100; i++) {
+  if( i % 3 === 0 && i % 5 === 0 ) {
+    console.log("fizzbuzz");
+  } else if (i % 3 === 0) {
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    console.log("buzz");
+  } else {
+    console.log(i);
+  }
 }
