1ee7b5d27103af8c0e8f7b579d232c54639e27da
ryankoch13 <ryan.koch13@gmail.com>
2020-07-01 10:37:52 -0700

    3rd challenge

--- migryanselam-loops.js
+++ migryanselam-loops.js
@@ -33,8 +33,27 @@
 // }
 // console.log(low);
 
-var nums = [3, 57, -9, 20, 67]
-for (let i=0; i<nums.length; i++){
-var remainder = nums[i]%2
-console.log(remainder);
+// var nums = [3, 57, -9, 20, 67]
+// for (let i=0; i<nums.length; i++){
+// var remainder = nums[i]%2
+// console.log(remainder);
+// }
+
+// var myString = "learn student";
+// var myArray = myString.split("");
+// var counter = 0;
+// // console.log(myArray);
+// for ( i=0 ; i<myArray.length ; i++ ) {
+//     if (myArray[i] === "e") {
+//         counter++;
+//     }
+// }
+// console.log(counter);
+
+for (let i = 0; i <= 15; i++) {
+      if (i % 2 !== 0) {
+        console.log(i + " IS ODD!");
+    } else {
+      console.log(i + " IS EVEN!");
+    }
 }
