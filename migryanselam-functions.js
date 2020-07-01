0e81c02635017b1de29b7f037740b28c9efefcc2
selamhabteab <selamhabteab@gmail.com>
2020-07-01 13:43:20 -0700

    1-2 challenges completed

---
+++ migryanselam-functions.js
@@ -0,0 +1,20 @@
+//Function Challenges
+//(1) Write a function named marco that returns "polo".
+ const marco = () => {
+   return "polo"
+ }
+ console.log(marco());
+//(2) Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+const greeting = (name) => {
+  return `Welcome, ${name}!`
+}
+console.log(greeting("Miguel, Ryan, and Selam"));
+//(3) Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+//const oddOrEven
+//(4) Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+
+//(5) Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
+
+//(6) Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+//(7) Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
