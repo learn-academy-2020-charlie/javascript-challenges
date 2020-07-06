eaeea81e23ad1922cd261419b5962e8ac468fc6f
selamhabteab <selamhabteab@gmail.com>
2020-07-06 16:43:07 -0700

    completed challenge 4

--- selry-object.js
+++ selry-object.js
@@ -53,3 +53,20 @@ const breakdown = (object) => {
 }
 
 console.log(breakdown(lunch))
+
+//CHALLENGE 4
+var animals = [
+  { name: "Waffles", type: "dog", age: 12 },
+  { name: "Fluffy", type: "cat", age: 14 },
+  { name: "Spelunky", type: "dog", age: 4 },
+  { name: "Hank", type: "cat", age: 11 },
+]
+//Create a function that takes in any array of objects and returns a new array with only those of type cat.
+
+const pets = (array) => {
+  let onlyCats = array.filter (value => {
+    return value.type === "cat"
+})
+    return onlyCats.map (value => value.name)
+}
+console.log(pets(animals))
