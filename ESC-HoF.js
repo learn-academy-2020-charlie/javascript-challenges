73fb0b7e786dd78eb6301fbf430ca57e35ca3e4b
cynavago <cnavarrohdz@gmail.com>
2020-07-06 16:13:59 -0700

    challenges 1 and 2

--- ESC-HoF.js
+++ ESC-HoF.js
@@ -1,62 +1,62 @@
 
-// //Challenge #1
-// // Use .map() or .filter() to complete the following exercises:
+Challenge #1
+Use .map() or .filter() to complete the following exercises:
 
-// // Write a function that takes in an array and returns a new array with all numbers multiplied by 10.
+Write a function that takes in an array and returns a new array with all numbers multiplied by 10.
 
-// // Expected output --> [30, 90, 150, 40, 100]
-// //
-// // var arr1 = [3, 9, 15, 4, 10]
-// // //Create a function that takes in an array
-// // const newArr = (array) => {
-// // //Create a higher order function map for the array and save it as a variable
-// // //Use map to multiply each value by 10
-// //   let mult10 = array.map(value => value*10)
-// //   //Return all numbers multipled by 10
-// //   return mult10
-// // }
-// // console.log(newArr(arr1));
+Expected output --> [30, 90, 150, 40, 100]
 
-// // CHALLENGE 2
-// // // Write a function that takes in an array and returns a new array with only odd numbers. Expected output --> [7, 3, 5, 13]
+var arr1 = [3, 9, 15, 4, 10]
+//Create a function that takes in an array
+const newArr = (array) => {
+//Create a higher order function map for the array and save it as a variable
+//Use map to multiply each value by 10
+  let mult10 = array.map(value => value*10)
+  //Return all numbers multipled by 10
+  return mult10
+}
+console.log(newArr(arr1));
 
-// // var arr2 = [2, 7, 3, 5, 8, 10, 13]
+CHALLENGE 2
+// Write a function that takes in an array and returns a new array with only odd numbers. Expected output --> [7, 3, 5, 13]
 
-// // //Create a function to accepts an array
-// // const newArray = (array) => {
-// // //Create a variable to hold new filtered array to draw odd numbers using modulo (%2!==0)
-// //   let oddNums = array.filter (value => {
-// // //Utilize the filter method to draw odd numbers
-// //     return value %2 !==0
-// //   })
-// // //Return new filtered array
-// //   return oddNums
-// // }
-// // console.log(newArray(arr2))
+var arr2 = [2, 7, 3, 5, 8, 10, 13]
 
-// // Challenge 3
-// // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use typeof method
-// var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"] 
-// // Expected output --> "nicework"
+//Create a function to accepts an array
+const newArray = (array) => {
+//Create a variable to hold new filtered array to draw odd numbers using modulo (%2!==0)
+  let oddNums = array.filter (value => {
+//Utilize the filter method to draw odd numbers
+    return value %2 !==0
+  })
+//Return new filtered array
+  return oddNums
+}
+console.log(newArray(arr2))
 
-// // create a function that accepts any array
-// const findLetters = (array) => {
-//   // create a variable to hold a new filtered array
-//   let letters = array.filter(value => {
-//     // use typeof method using "string" to get the letters in the  new array
-//     return typeof value === "string"
-//   })
-//   // use join to convert the new array into the string and return it
-//   return letters.join("")
-// }
-// //call the function
-// console.log(findLetters(comboArr));
+// Challenge 3
+// Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use typeof method
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"] 
+// Expected output --> "nicework"
+
+// create a function that accepts any array
+const findLetters = (array) => {
+  // create a variable to hold a new filtered array
+  let letters = array.filter(value => {
+    // use typeof method using "string" to get the letters in the  new array
+    return typeof value === "string"
+  })
+  // use join to convert the new array into the string and return it
+  return letters.join("")
+}
+//call the function
+console.log(findLetters(comboArr));
 
 
 
-// STRETCH Challenge #1
-// Create a function that takes in a string and returns a new string with all the vowels removed.
-// Expected output --> "jvscrpt s wsm"
+STRETCH Challenge #1
+Create a function that takes in a string and returns a new string with all the vowels removed.
+Expected output --> "jvscrpt s wsm"
 
 var str = "javascript is awesome"
 
---
+++ MC-objects.js
@@ -0,0 +1,37 @@
+//Challenge #1
+// Consider this variable:
+// Write the code that accesses the first name of the person object.
+// Write the code that accesses the last name of the person object.
+// Write the code that gives the person object a property of homePlanet and set it to 'Earth'.
+// Update the person object with a method that logs "Arthur Dent is from planet Earth".
+// Consider this variable:
+
+// var person = {
+//   firstName: "Arthur",
+//   lastName: "Dent",
+//   homePlanet: "Earth",
+
+//   whereYouFrom: function(){
+//       return `${this.firstName} ${this.lastName} is from planet ${this.homePlanet}`
+//   }
+// }
+
+// console.log(person.firstName);
+// console.log(person.lastName);
+// console.log(person.whereYouFrom());
+
+// Challenge #2
+// Consider this variable:
+// Write a function called describeProduct takes product as an argument and logs "The product is a chair. It costs $14.99".
+
+// var product = {
+//   name: "chair", price: 14.99
+// }
+
+// //write a function called describeProduct and takes an argument
+// const describeProduct = (product) => {
+//     return `The product is a ${product.name}. It costs ${product.price}.`
+// }
+
+// console.log(describeProduct(product));
+
