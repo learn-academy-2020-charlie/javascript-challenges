1491634292605cf872fd769b29aa0d3fea2ae40e
cynavago <cnavarrohdz@gmail.com>
2020-07-08 16:42:56 -0700

    latest upload

--- miguel-cynthia/challenges.test.js
+++ miguel-cynthia/challenges.test.js
@@ -112,4 +112,171 @@ const { TestScheduler } = require("jest")
 // }
 
 //CHALLENGE 5
-Write the test for a function that takes in one argument(number) and returns whether the number is odd. Write the function that will make the test pass.
+// Write the test for a function that takes in one argument(number) and returns whether the number is odd. Write the function that will make the test pass.
+
+//write a test for a function that takes in a number and determines if its odd
+// describe("oddNum", () =>{
+//     test("return whether the number is odd", () =>{
+//         expect(oddNum(35)).toEqual("odd")
+//         expect(oddNum(34)).toEqual("even")
+//     })
+// })
+
+// const oddNum = (number) => {
+//     if(number % 2 !== 0){
+//         return "odd"
+//     } else {
+//         return "even"
+//     }
+// }
+
+// CHALLENGE 6
+// Write the test for a function that takes in a fruit and returns yellow if the argument is banana, red if apple and purple if grape. Write the function that will make the test pass.
+
+// describe("fruitColor", () =>{
+//     test("return color of the fruit", () =>{
+//         expect(fruitColor("banana")).toEqual("yellow")
+//         expect(fruitColor("apple")).toEqual("red")
+//         expect(fruitColor("grape")).toEqual("purple")
+//     })
+// })
+
+// const fruitColor = (fruitname) =>{
+//     if(fruitname === "banana"){
+//         return "yellow"
+//     } else if (fruitname === "apple"){
+//         return "red"
+//     } else if (fruitname === "grape"){
+//         return "purple"
+//     }
+
+// Challenge 7
+// Write the test for a function called Rick that returns "Morty". Write the function that will make the test pass.
+
+// describe("rick", () =>{
+//     test("return Morty", () =>{
+//         expect(rick()).toEqual("Morty")
+//     })
+// })
+
+// const rick = () =>{
+//     return "Morty"
+// }
+
+// CHALLENGE 8
+// Write the test for a function called greeter that takes a name as an argument and returns a greeting with that name to the screen. Write the function that will make the test pass.
+
+// describe("greeter", () =>{
+//     test("returns a string of a greeting with a name", () =>{
+//         expect(greeter(name)).toEqual(`Hello ${name}`)
+//     })
+// })
+
+// var name = "Cynthia"
+
+// const greeter = (name) =>{
+//     return `Hello ${name}`
+// }
+
+// Challenge #9
+// Write the test for a function called oddOrEven that takes an number as an argument and logs whether the number is odd or even. Write the function that will make the test pass.
+
+// describe("oddOrEven", () =>{
+//     test("returns whether number is odd or even", () => {
+//         expect(oddOrEven(num1)).toEqual("odd")
+//         expect(oddOrEven(num2)).toEqual("even")
+
+//     })
+// })
+
+// var num1 = 43
+// var num2 = 44
+
+// const oddOrEven = (nums) =>{
+//     if(nums %2 !== 0){
+//         return "odd"
+//     } else{
+//         return "even"
+//     }
+// }
+
+// Challenge #10
+// Write the test for a function called doubler that takes a number and returns the result of the number multiplied by 2. Write the function that will make the test pass.
+
+// describe("doubler", () =>{
+//     test("returns result of number * 2", () =>{
+//         expect(doubler(num1)).toEqual(num1*2)
+//     })
+// })  
+
+// var num1 = 3
+
+// const doubler = (number) =>{
+//     return number * 2
+// }
+
+// Challenge #11
+// Write the test for a function called multiply that takes two numbers as arguments and logs the result of one of the numbers multiplied by the other. Write the function that will make the test pass.
+
+// describe("multiply", () =>{
+//     test("returns result of 2 numbers multiplied by the other", () => {
+//         expect(multiply(num1, num2)).toEqual(num1*num2)
+//     })
+// })
+
+// var num1 = 3
+// var num2 = 5
+
+// const multiply = (arg1, arg2) =>{
+//     return arg1*arg2
+// }
+
+// Challenge #12
+// Write the test for a function called divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5". Write the function that will make the test pass.
+
+// describe("divisibleBy", () => {
+//     test("returns string whether num1 is divisible by num2", () => {
+//         expect(divisibleBy(num1, num2)).toEqual(`${num1} is evenly divisible by ${num2}`)
+//     })
+// })
+
+// var num1 = 10
+// var num2 = 5
+// // var num3 = 17
+// // var num4 = 8
+
+// const divisibleBy = (arg1, arg2) =>{
+//     if(arg1 % arg2 === 0){
+//         return `${arg1} is evenly divisible by ${arg2}`
+// }}
+
+// Challenge #13
+// Write the test for a function called fizzbuzz. If a number is a multiple of 3, replace it with the word 'fizz'. If a number is a multiple of five, replace it with the word 'buzz'. If a number is a multiple of both 3 and 5, replace it with 'fizzbuzz'. Write the function that will make the test pass.
+
+// describe("fizzbuzz", () => {
+//     test("return fizz if # is multiple of 3, buzz if # is multiple of 5, and fizzbuzz if # is multiple of 3 and 5", () => {
+//         expect(fizzbuzz(num1)).toEqual("fizz")
+//         expect(fizzbuzz(num2)).toEqual("buzz")
+//         expect(fizzbuzz(num3)).toEqual("fizzbuzz")
+//     })
+// })
+
+// var num1 = 27
+// var num2 = 20
+// var num3 = 15
+
+// const fizzbuzz = (num) =>{
+//     if (num%3 === 0 && num%5 === 0){
+//     return "fizzbuzz"
+//     } else if(num % 3 === 0){
+//         return "fizz"
+//     } else if (num % 5 === 0){
+//         return "buzz" 
+//     }
+// }
+
+
+
+
+
+
