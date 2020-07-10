428c776ca51f629ed63cd03ce6b1617954273a99
ericagates <ericajoangates@gmail.com>
2020-07-10 11:55:45 -0700

    color box challenge

--- selmig-colorbox/colorbox-app/src/Button.js
+++ selmig-colorbox/colorbox-app/src/Button.js
@@ -4,8 +4,30 @@ import React, {Component} from 'react';
 class Button extends Component {
   constructor (props) {
     super (props)
+    this.state = {
+      currentColor: "click to change color",
+      colorOptions: ["green", "blue", "yellow", "red", "purple", "orange"]
+
+    }
+  }
+
+  handleChange = () => {
+    let { colorOptions, currentColor } = this.state
+    let newColor = colorOptions[Math.floor(Math.random()*colorOptions.length)]
+    this.setState({ currentColor: newColor })
 
   }
+
+  render () {
+    let { currentColor } = this.state
+    
+    return (
+      <div onClick={this.handleChange} style =  {{width: 100, height: 100, border: '1px solid black',  backgroundColor: currentColor}}>
+      { currentColor }
+      </div>
+  )
+
+  } 
 }
 
-export default Button;
+export default Button
