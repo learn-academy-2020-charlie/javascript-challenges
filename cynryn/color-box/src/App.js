e81630f087783ceacbcc89a440ea8ad007e50102
cynavago <cnavarrohdz@gmail.com>
2020-07-10 11:07:35 -0700

    boxes

--- cynryn/color-box/src/App.js
+++ cynryn/color-box/src/App.js
@@ -5,27 +5,107 @@ class App extends Component{
   constructor(props){
     super(props)
       this.state = {
-        currentColor: "",
+        //randomize currentColor so that it can work independently on different buttons
+        currentColor1: "",
+        currentColor2: "",
+        currentColor3: "",
+        currentColor4: "",
+        currentColor5: "",
+        currentColor6: "",
+
         colorOptions: ["green", "blue", "yellow", "red", "purple", "orange"]
       }
   }
-  handleChange = () => {
+
+  handleChange1 = () => {
     let { colorOptions } = this.state;
     let newColor = colorOptions[Math.floor(Math.random() * 6)]
     let newColorString = `${ newColor }`
-    return this.setState({ currentColor: newColorString })
+    return this.setState({ currentColor1: newColorString })
   }
+
+  handleChange2 = () => {
+    let { colorOptions } = this.state;
+    let newColor = colorOptions[Math.floor(Math.random() * 6)]
+    let newColorString = `${ newColor }`
+    return this.setState({ currentColor2: newColorString })
+  }
+
+  handleChange3 = () => {
+    let { colorOptions } = this.state;
+    let newColor = colorOptions[Math.floor(Math.random() * 6)]
+    let newColorString = `${ newColor }`
+    return this.setState({ currentColor3: newColorString })
+  }
+
+  handleChange4 = () => {
+    let { colorOptions } = this.state;
+    let newColor = colorOptions[Math.floor(Math.random() * 6)]
+    let newColorString = `${ newColor }`
+    return this.setState({ currentColor4: newColorString })
+  }
+
+  handleChange5 = () => {
+    let { colorOptions } = this.state;
+    let newColor = colorOptions[Math.floor(Math.random() * 6)]
+    let newColorString = `${ newColor }`
+    return this.setState({ currentColor5: newColorString })
+  }
+
+  handleChange6 = () => {
+    let { colorOptions } = this.state;
+    let newColor = colorOptions[Math.floor(Math.random() * 6)]
+    let newColorString = `${ newColor }`
+    return this.setState({ currentColor6: newColorString })
+  }
+
 
   render(){ 
     return(
       <>
       <h1><u>The Ultimate Color Selector!</u></h1>
-      <button onClick = { this.handleChange }>
-        CHOOSE YOUR COLOR
-      </button>
+
+
+      <button id = "testbutton" onClick = { this.handleChange1 }>
       <div>
-        { this.state.currentColor }
+        { this.state.currentColor1 }
       </div>
+      </button>
+
+      <button id = "testbutton" onClick = { this.handleChange2 }>
+      <div>
+      { this.state.currentColor2 }
+      </div>
+      </button>
+
+      <button id = "testbutton" onClick = { this.handleChange3 }>
+      <div>
+      { this.state.currentColor3 }
+      </div>
+      </button>
+
+      <button id = "testbutton" onClick = { this.handleChange4 }>
+      <div>
+      { this.state.currentColor4 }
+      </div>
+      </button>
+
+      <button id = "testbutton" onClick = { this.handleChange5 }>
+      <div>
+      { this.state.currentColor5 }
+      </div>
+      </button>
+
+      <button id = "testbutton" onClick = { this.handleChange6 }>
+      <div>
+      { this.state.currentColor6 }
+      </div>
+      </button>
+
+
+      {/* <div>
+        { this.state.currentColor }
+      </div> */}
       </>
     )
     }
