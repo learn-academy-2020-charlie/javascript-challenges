3e411f83c996fc5f9c4a825d25a2a1947785fb6d
rmdpalo <rmd.palo@gmail.com>
2020-07-13 12:03:43 -0700

    Erica and Miguel's Dice Roller

---
+++ EM-DiceRoller/dice-roller/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Dice from './components/dice'
+import Rolls from './components/rolls'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+
+  }
+
+}
+export default App;
