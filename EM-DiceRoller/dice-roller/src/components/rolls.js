be06134c2dadc97a4fe7dbea33fdf31b16228d71
rmdpalo <rmd.palo@gmail.com>
2020-07-13 15:39:17 -0700

    finished dice roller

--- EM-DiceRoller/dice-roller/src/components/rolls.js
+++ EM-DiceRoller/dice-roller/src/components/rolls.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 export default class Rolls extends Component {
     render (){
-        return 
+        return (
+            <div>
+                 {/* This is the JSX and styling for the previous rolls */}
+                <h2>Previous Rolls:</h2>
+                <div>
+                    { this.props.previousRolls }
+                </div>
+            </div>
+        )
     }
 }
